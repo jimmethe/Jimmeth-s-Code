67db63d52e2ee151737e746480a72b8caa7fdc33
jimmeth <you@example.com>
2013-05-25 10:03:42 +1000

    Yays

---
+++ AndroidStudioProjects/TESTAPP/TESTAPP/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.testapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public final static String EXTRA_MESSAGE = "com.example.testapp.MESSAGE";
+
+public void sendMessage(View view){
+
+    CheckBox check = (CheckBox) findViewById(R.id.checkBox);
+    TextView text = (TextView) findViewById(R.id.textView);
+if (check.isChecked()){
+                text.setText("cheese");
+}
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
